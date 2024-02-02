3fa52d50dfd281237a1c1071053107a58deb4d19
rowm-io <official.romio23@gmail.com>
2024-02-02 17:09:31 +0530

    Fixed linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
+
 def add(a, b):
     """Add a and b, and return the result"""
     return a + b
+
 
 def subtract(a, b):
     """Subtract b from a and return the result"""
