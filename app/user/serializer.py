03925529cb8d8b14917a75afa7a10f0a88838ea7
rowm-io <official.romio23@gmail.com>
2024-02-22 01:55:33 +0530

    Fixed linting issue

--- app/user/serializer.py
+++ app/user/serializer.py
@@ -9,12 +9,13 @@ from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
 
     class Meta:
         model = get_user_model()
-        fields = ['email','password','name']
+        fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, vallidated_data):
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,9 +12,11 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user"""
     return get_user_model().objects.create_user(**params)
+
 
 class PublicUserApiTests(TestCase):
     """Test the public features of the user API"""
@@ -146,5 +148,3 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-
